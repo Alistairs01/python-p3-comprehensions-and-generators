9ac999af06b39122c540248d94eb072fb6207930
Alistairs <alistairs.gatheru@student.moringaschool.com>
2024-05-25 11:49:32 +0300

    comprehension

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    #Return even number in the num_list by checking if num divided by two has a remainder of 0
+    return [n for n in num_list if n % 2 == 0]
+print(return_evens)
+
+        
+    
 
 def make_exclamation(sentence_list):
-    pass
+    # Adds the exclamation to each string in sentence_list
+    return [string + '!' for string in sentence_list]
+print(make_exclamation)
+    
